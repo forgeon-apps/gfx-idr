3cd4620c21e752a4c92590b43c66c5b83a52af01
forgeon-apps <forgeon.apps@gmail.com>
2026-06-10 17:38:24 +0700

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,29 @@ import React from 'react';
 
 import { GetStaticProps } from 'next';
 
-import { BlogGallery, IBlogGalleryProps } from '../blog/BlogGallery';
-import { Meta } from '../layout/Meta';
+import { GoogleFxSearchMock } from '@/components/google-fx/GoogleFxSearchMock';
+
+import { IBlogGalleryProps } from '../blog/BlogGallery';
+// import { Meta } from '../layout/Meta';
 import { IPaginationProps } from '../pagination/Pagination';
-import { Main } from '../templates/Main';
+// import { Main } from '../templates/Main';
 import { AppConfig } from '../utils/AppConfig';
 import { getAllPosts } from '../utils/Content';
 
-const Index = (props: IBlogGalleryProps) => (
-  <Main
-    meta={
-      <Meta
-        title="Made with Next.js, TypeScript, ESLint, Prettier, PostCSS, Tailwind CSS"
-        description={AppConfig.description}
-      />
-    }
-  >
-    <BlogGallery posts={props.posts} pagination={props.pagination} />
-  </Main>
-);
+// const Index = (props: IBlogGalleryProps) => (
+//   <Main
+//     meta={
+//       <Meta
+//         title="Made with Next.js, TypeScript, ESLint, Prettier, PostCSS, Tailwind CSS"
+//         description={AppConfig.description}
+//       />
+//     }
+//   >
+//     <BlogGallery posts={props.posts} pagination={props.pagination} />
+//   </Main>
+// );
+
+const Index = () => <GoogleFxSearchMock />;
 
 export const getStaticProps: GetStaticProps<IBlogGalleryProps> = async () => {
   const posts = getAllPosts(['title', 'date', 'slug']);
